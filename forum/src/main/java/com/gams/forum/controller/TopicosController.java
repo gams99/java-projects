5560202a2b39399f3b68c3c310dac968f6a82e8f
gams <gabriel.antonioms@gmail.com>
2021-02-01 00:19:28 -0300

    created filter nomeCurso

--- forum/src/main/java/com/gams/forum/controller/TopicosController.java
+++ forum/src/main/java/com/gams/forum/controller/TopicosController.java
@@ -1,14 +1,12 @@
 package com.gams.forum.controller;
 
 import com.gams.forum.controller.dto.TopicoDto;
-import com.gams.forum.model.Curso;
 import com.gams.forum.model.Topico;
 import com.gams.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -18,8 +16,14 @@ public class TopicosController {
     private TopicoRepository topicoRepository;
 
     @RequestMapping("/topicos")
-    public List<TopicoDto> lista(){
-        List<Topico> topicos = topicoRepository.findAll();
-        return TopicoDto.converter(topicos);
+    public List<TopicoDto> lista(String nomeCurso){
+
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso); //this method is created with TopicoRepository helps,
+            return TopicoDto.converter(topicos); // example =>   topicos?nomeCurso=Spring+Boot
+        }
     }
 }
--- forum/src/main/java/com/gams/forum/repository/TopicoRepository.java
+++ forum/src/main/java/com/gams/forum/repository/TopicoRepository.java
@@ -3,5 +3,8 @@ package com.gams.forum.repository;
 import com.gams.forum.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+    List<Topico> findByCursoNome(String nomeCurso); //Jpa searching automatically by class Curso and attribute Nome
 }
