71ca7b9f874735f6e1d704d3a1f70eec91fd4c6f
gams <gabriel.antonioms@gmail.com>
2021-02-03 21:23:30 -0300

    upgraded in pagination and add ordenation

--- forum/src/main/java/com/gams/forum/ForumApplication.java
+++ forum/src/main/java/com/gams/forum/ForumApplication.java
@@ -3,8 +3,10 @@ package com.gams.forum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport
 @EntityScan("com.gams.forum.model")
 public class ForumApplication {
 
