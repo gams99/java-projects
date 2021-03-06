0c3c03a8193d9d4b88f110f6bde90b92e3087a2e
gams <gabriel.antonioms@gmail.com>
2021-03-05 22:05:24 -0300

    using profile test in configuration

--- forum/src/test/java/com/gams/forum/repository/CursoReposityTest.java
+++ forum/src/test/java/com/gams/forum/repository/CursoReposityTest.java
@@ -4,17 +4,32 @@ import com.gams.forum.model.Curso;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class CursoReposityTest {
 
     @Autowired
     private CursoRepository repository;
 
+    @Autowired
+    private TestEntityManager em;
+
     @Test
     public void shouldFindByNome() {
+
         String nomeCurso = "HTML 5";
+        
+        Curso html5 = new Curso();
+        html5.setNome(nomeCurso);
+        html5.setCategoria("Programacao");
+        em.persist(nomeCurso);
+
         Curso curso = repository.findByNome(nomeCurso);
         Assertions.assertNotNull(curso);
         Assertions.assertEquals(nomeCurso, curso.getNome());
