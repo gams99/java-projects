ac5432aacfaf3eb858fd6874234ae8258f9633a7
gams <gabriel.antonioms@gmail.com>
2021-03-09 00:55:46 -0300

    add avaliador tests

--- teste-de-unidade/src/br/com/gams/leilao/dominio/Avaliador.java
+++ teste-de-unidade/src/br/com/gams/leilao/dominio/Avaliador.java
@@ -1,15 +1,30 @@
 package br.com.gams.leilao.dominio;
 
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 public class Avaliador {
 
     private Double maiorDeTodos = Double.NEGATIVE_INFINITY;
     private Double menorDeTodos = Double.POSITIVE_INFINITY;
+    private double media;
+    private double total;
 
     public void avalia(Leilao leilao){
         for(Lance lance : leilao.getLances()){
             if(lance.getValor() > maiorDeTodos) maiorDeTodos = lance.getValor();
             if(lance.getValor() < menorDeTodos) menorDeTodos = lance.getValor();
+            total+= lance.getValor();
         }
+        if(total == 0) {
+            media = 0;
+            return;
+        }
+        media = total/leilao.getLances().size();
+    }
+
+    public Double getMedia() {
+        return media;
     }
 
     public Double getMaiorLance() {
--- teste-de-unidade/src/br/com/gams/leilao/test/TesteDoAvaliador.java
+++ teste-de-unidade/src/br/com/gams/leilao/test/TesteDoAvaliador.java
@@ -10,6 +10,8 @@ import org.junit.Test;
 
 public class TesteDoAvaliador {
 
+
+
     @Test
     public void deveEntenderLancesEmOrdemCrescente(){
         Usuario jose = new Usuario("Jose");
@@ -18,18 +20,58 @@ public class TesteDoAvaliador {
 
         Leilao leilao = new Leilao("PS5");
 
+        leilao.propoe(new Lance(maria, 300.0));
         leilao.propoe(new Lance(joao, 400.0));
-        leilao.propoe(new Lance(jose, 700.0));
-        leilao.propoe(new Lance(maria, 230.0));
+        leilao.propoe(new Lance(jose, 500.0));
+
 
         Avaliador leiloeiro = new Avaliador();
         leiloeiro.avalia(leilao);
 
-        double maiorEsperado = 700.0;
-        double menorEsperado = 230.0;
+        double maiorEsperado = 500.0;
+        double menorEsperado = 300.0;
 
         Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001);
-
         Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+    }
+
+    @Test
+    public void deveCalcularMedia(){
+
+        Usuario jose = new Usuario("Jose");
+        Usuario joao = new Usuario("Joao");
+        Usuario maria = new Usuario("Maria");
+
+        Leilao leilao = new Leilao("PS5");
+
+        leilao.propoe(new Lance(maria, 300.0));
+        leilao.propoe(new Lance(joao, 400.0));
+        leilao.propoe(new Lance(jose, 500.0));
+
+
+        Avaliador leiloeiro = new Avaliador();
+        leiloeiro.avalia(leilao);
+
+        double mediaEsperada = 400.0;
+
+        System.out.println(leiloeiro.getMedia());
+        Assert.assertEquals(mediaEsperada, leiloeiro.getMedia(), 0.00001);
+    }
+
+    @Test
+    public void testaMediaDeZeroLance(){
+
+        // cenario
+        Usuario ewertom = new Usuario("Ewertom");
+
+        // acao
+        Leilao leilao = new Leilao("Iphone 7");
+
+        Avaliador avaliador = new Avaliador();
+        avaliador.avalia(leilao);
+
+        //validacao
+        Assert.assertEquals(0, avaliador.getMedia(), 0.0001);
+
     }
 }
