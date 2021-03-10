3e6939ca8415a29e16c9f0366c99c49e0bba5847
gams <gabriel.antonioms@gmail.com>
2021-03-10 09:20:49 -0300

    add new test for LeilaoTest

--- teste-de-unidade/src/br/com/gams/leilao/test/LeilaoTest.java
+++ teste-de-unidade/src/br/com/gams/leilao/test/LeilaoTest.java
@@ -32,4 +32,45 @@ public class LeilaoTest {
         assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
         assertEquals(3000.0, leilao.getLances().get(1).getValor(), 0.00001);
     }
+    @Test
+    public void deveReceber2LancesSeguidosDoMesmoUsuario(){
+        Leilao leilao = new Leilao("Mackbook Pro 15");
+        Usuario steveJobs = new Usuario("Steve Jobs");
+        leilao.propoe(new Lance(steveJobs, 2000));
+        leilao.propoe(new Lance(steveJobs, 3000));
+
+        assertEquals(1, leilao.getLances().size());
+        assertEquals(2000.0, leilao.getLances().get(0).getValor(), 0.00001);
+    }
+
+    @Test
+    public void naoDeveAceitarMaisDoQue5LancesDeUmMesmoUsuario(){
+        Leilao leilao = new Leilao("Mackbook Pro 15");
+        Usuario steveJobs = new Usuario("Steve Jobs");
+        Usuario billGates = new Usuario("Bill Gates");
+
+        leilao.propoe(new Lance(steveJobs, 2000));
+        leilao.propoe(new Lance(billGates, 3000));
+
+        leilao.propoe(new Lance(steveJobs, 4000));
+        leilao.propoe(new Lance(billGates, 5000));
+
+        leilao.propoe(new Lance(steveJobs, 6000));
+        leilao.propoe(new Lance(billGates, 7000));
+
+        leilao.propoe(new Lance(steveJobs, 7500));
+        leilao.propoe(new Lance(billGates, 7800));
+
+        leilao.propoe(new Lance(steveJobs, 8000));
+        leilao.propoe(new Lance(billGates, 8500));
+
+        //deve ser ignorado
+        leilao.propoe(new Lance(steveJobs, 9000));
+
+        assertEquals(10, leilao.getLances().size());
+        assertEquals(8500.0, leilao.getLances().get(leilao.getLances().size()-1).getValor(), 0.00001);
+
+
+
+    }
 }
